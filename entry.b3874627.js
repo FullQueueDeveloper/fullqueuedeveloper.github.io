e06d8d7c1c98bb0b3b481ce5ead7dca28dae0eea
FullQueueDeveloper <FullQueueDeveloper@users.noreply.github.com>
2022-05-19 17:49:07 +0000

    deploy: e6ec267083347a7e0e9cb23b22c249706714bcdc

---
+++ entry.b3874627.js
@@ -0,0 +1,1 @@
+window.onload=function(){window.location.href="webcal://p41-caldav.icloud.com/published/2/MjA1NDYxNjQwMTMyMDU0NoJpIuCToGGes2f8Qq8Nhy9hq3cM6hgm91tsbUg4N2AiXr_psiv6jIjGyt-6KqvDgzPG5_qH20JztZkMytmk2gQ"};
